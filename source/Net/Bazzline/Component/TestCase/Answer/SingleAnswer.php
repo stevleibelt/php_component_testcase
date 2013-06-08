2579ddf31a9e646ffc3e20ad8c203ac0f518bb8b
stevleibelt <stev@leibelt.de>
2013-06-08 20:09:16 +0200

    Moved logic into getPercentageOfAccuracy

--- source/Net/Bazzline/Component/TestCase/Answer/SingleAnswer.php
+++ source/Net/Bazzline/Component/TestCase/Answer/SingleAnswer.php
@@ -17,21 +17,13 @@ class SingleAnswer extends AnswerAbstract
 {
     /**
      * @{inheritDoc}
-     * @todo move logic into getPercentageOfAccuracy
-     */
-    public function validateSelectedOpportunities()
-    {
-        $isValid = ((count($this->selectedOpportunities) == 1)
-            && (in_array(end($this->selectedOpportunities), $this->validOpportunities)));
-
-        return $isValid;
-    }
-
-    /**
-     * @{inheritDoc}
      */
     public function getPercentageOfAccuracy()
     {
-        return ($this->validateSelectedOpportunities()) ? 100 : 0;
+        $percentage = ((count($this->selectedOpportunities) == 1)
+            && (in_array(end($this->selectedOpportunities), $this->validOpportunities))) ?
+            100 : 0;
+
+        return $percentage;
     }
 }
