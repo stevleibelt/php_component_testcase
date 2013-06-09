63d0304b058ea4c4107cbac1471ff18eb9be95d9
stevleibelt <stev@leibelt.de>
2013-06-09 23:57:32 +0200

    Started FactoryAbstractTest

---
+++ test/Net/Bazzline/Component/TestCase/Factory/FactoryAbstractTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-06-09 
+ */
+
+namespace Test\Net\Bazzline\Component\TestCase\Factory;
+
+use Test\Net\Bazzline\Component\TestCase\UnitTestCase;
+use Test\Net\Bazzline\Component\TestCase\MockFactory;
+
+class FactoryAbstractTest extends UnitTestCase
+{
+
+}
