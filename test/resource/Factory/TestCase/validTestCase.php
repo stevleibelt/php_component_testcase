c3ae4d490e14ec178d1a743342161ad71040fe5a
stevleibelt <stev@leibelt.de>
2013-06-13 22:04:47 +0200

    Added resource for factory test testCase

---
+++ test/resource/Factory/TestCase/validTestCase.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-06-13 
+ */
+return array(
+    'question' => '../Question/validQuestion.json',
+    'answer' => '../Answer/validAnswer.yaml'
+);
