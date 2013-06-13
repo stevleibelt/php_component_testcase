e16e9e83a7b2b8e329a97b08239e858d4f0751bc
stevleibelt <stev@leibelt.de>
2013-06-13 22:23:41 +0200

    Added resources for suite factory test

---
+++ test/resource/Factory/Suite/validSuite.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-06-13 
+ */
+return array(
+    'name' => 'Valid Suite',
+    'language' => 'en',
+    'description' => 'This is a suite for unittest reasons. Do not expect usefull stuff in here.',
+    'pathToTestCases' => array(
+        '../TestCase/validTestCase.php',
+        '../TestCase/validTestCase.json',
+        '../TestCase/validTestCase.yaml'
+    )
+);
