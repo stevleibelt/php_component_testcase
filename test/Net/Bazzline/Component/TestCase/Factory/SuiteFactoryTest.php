f05d4ee8b902633547be58215900cf361e44b8ef
stevleibelt <stev@leibelt.de>
2013-06-13 22:14:53 +0200

    Added "invalid source test"

--- test/Net/Bazzline/Component/TestCase/Factory/SuiteFactoryTest.php
+++ test/Net/Bazzline/Component/TestCase/Factory/SuiteFactoryTest.php
@@ -26,4 +26,33 @@ class SuiteFactoryTest extends UnitTestCase
         );
     }
 
+    /**
+     * @return array
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-13
+     */
+    public static function invalidSourceTypeProvider()
+    {
+        return array(
+            array(null),
+            array(1),
+            array(''),
+            array(new stdClass())
+        );
+    }
+
+    /**
+     * @dataProvider invalidSourceTypeProvider
+     * @expectedException \Net\Bazzline\Component\TestCase\Factory\InvalidArgumentException
+     * @expectedExceptionMessage No valid source file given
+     *
+     * @param mixed $source
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-13
+     */
+    public function testFromSourceWithInvalidType($source)
+    {
+        $factory = SuiteFactory::create();
+        $factory->fromSource($source);
+    }
 }
